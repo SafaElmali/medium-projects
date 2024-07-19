4c392f77dbd1e0120b34a1a2e8dc69bff9169143
SafaElmali <tsafaelmali@gmail.com>
2024-07-19 18:38:26 +0300

    remove onesignal demo

--- onesignal-demo/serverless/src/functions/scheduled-event-triggers/scheduled-message-sent/queries.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "graphql-request";
-
-export const UpdateScheduledMessageStatus = gql`
-  mutation UpdateScheduledMessageStatus($id: uuid!) {
-    update_message_by_pk(pk_columns: { id: $id }, _set: { status: "sent" }) {
-      id
-    }
-  }
-`;
